06848e310e5b779bbc066f207285ec26c743b5eb
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2024-12-16 20:54:33 +0200

    add routes for notes and note pages

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
 return [
 "routes"=>[
-        "/"=>"./app/Views/homepage.php",
-        "/signup"=>"./app/Views/signup.php",
-        "/Login"=>"./app/Views/Login.php",
-        "/notes"=>"./app/Views/notes.php",
+        "/"=>["./app/Views/homepage.php"],
+        "/signup"=>["./app/Views/signup.php"],
+        "/Login"=>["./app/Views/Login.php"],
         "/Login/process"=>["./app/Controllers/LoginController.php",$method="handleLogin"],
         "/signup/process"=>["./app/Controllers/SignupController.php",$method="handleSignup"],
+        "/notes/add"=>["./app/Controllers/NotesController.php",$method="addNote"],
+        "/notes"=>["./app/Controllers/NotesController.php",$method="showNotes"],
+        "/note"=>["./app/Views/note.php"]
 ]
 ];
