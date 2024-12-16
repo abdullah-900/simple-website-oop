16f14874a95859a2f8c3bbb4afbe84e4c5085702
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2024-12-16 21:03:19 +0200

    require all used classes in all website

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+require_once "./utils/Responses.php";
+require_once "./utils/functions.php";
+include_once "./utils/Validator.php";
+require_once "config/config_session.php";
 $Routes=require_once "config.php";
 require_once "Router.php";
 $router=new Router($Routes);
