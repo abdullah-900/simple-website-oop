6fb25020704e3911c591b2c3727477632862362c
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2024-12-24 05:33:20 +0200

    move index file to public folder

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "./utils/Responses.php";
-require_once "./utils/functions.php";
-include_once "./utils/Validator.php";
-require_once "config/config_session.php";
-$Routes=require_once "config.php";
-require_once "Router.php";
-$router=new Router($Routes);
-$router->Route();
