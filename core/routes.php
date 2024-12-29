cd529215ebc063d66c994f481ba7013bee46bd0f
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2024-12-29 18:56:37 +0200

    feat: update routing to include access control and enhance note management functionality

--- core/routes.php
+++ core/routes.php
@@ -1,11 +1,13 @@
 <?php
-$router->get('/','./app/Views/homepage.php');
-$router->get('/Login','./app/Views/Login.php',);
-$router->get('/signup','./app/Views/signup.php',);
-$router->get('/note','./app/Views/notes/show.php',);
-$router->get('/notes','./app/controllers/NotesController.php','showNotes');
-$router->post('/Login/process','./app/controllers/LoginController.php','handleLogin');
-$router->post('/signup/process','./app/controllers/SignupController.php','handleSignup');
-$router->post('/notes/add','./app/controllers/NotesController.php','addNote');
-$router->delete('/notes/delete','./app/controllers/NotesController.php','deleteNote');
+$router->get('/','./app/Views/homepage.php','guest');
+$router->get('/Login','./app/Views/Login.php','guest');
+$router->get('/signup','./app/Views/signup.php','guest');
+$router->get('/note','./app/Views/notes/show.php','user');
+$router->get('/notes','./app/controllers/NotesController.php','user','showNotes');
+$router->post('/Login/process','./app/controllers/LoginController.php','guest','handleLogin');
+$router->post('/signup/process','./app/controllers/SignupController.php','guest','handleSignup');
+$router->post('/notes/create','./app/controllers/NotesController.php','user','createNote');
+$router->delete('/Logout','./app/controllers/AuthController.php','user','logout');
+$router->delete('/notes/delete','./app/controllers/NotesController.php','user','deleteNote');
+$router->patch('/notes/patch','./app/controllers/NotesController.php','user','patchNote');
 
