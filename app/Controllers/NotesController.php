bacf598ea9d5b8d06a5596052deb2ee9bb73fde8
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2024-12-29 18:55:08 +0200

    feat: add patchNote method to NotesController for editing notes with validation

--- app/Controllers/NotesController.php
+++ app/Controllers/NotesController.php
@@ -1,11 +1,11 @@
 <?php
 declare (strict_types=1);
 use Core\Validator;
-require_once BASE_PATH ."./core/functions.php";
 use function Core\base_path;
+
 class NotesController extends Notes {
 
-
+private $validator;
 public function showNotes() {
    $notes = $this->getNotes();
 if (is_array($notes)) {
@@ -17,7 +17,7 @@ if (is_array($notes)) {
 require_once base_path("./app/Views/notes/index.php");;
 }
 
-public function addNote() {
+public function createNote() {
 
     if ($_SERVER["REQUEST_METHOD"]==="POST" && isset($this->userId)) {
         try {
@@ -51,6 +51,46 @@ public function deleteNote() {
         try {
         $noteId=$_POST["noteId"];
        $this->delNote($noteId);
+          header("Location: ../notes");
+        } catch (Exception $e) {
+            die("query failed" . $e->getMessage());
+        }
+    }else {
+        header("Location: ../notes");
+        die();
+    }
+}
+
+
+public function patchNote() {
+    if ($_SERVER["REQUEST_METHOD"]==="POST" && isset($this->userId)) {
+        try {
+        $noteId=$_POST["noteId"];
+        $title=$_POST["notetitle"];
+        $note=$_POST["note"];
+        $notes_errors=[];
+        if (!Validator::is_field_empty([$title,$note])) {
+            if (!Validator::checkStringLength($title,1,300)) {
+                $notes_errors["title"]="title must be no more than 300 characters";
+            }
+            if (!Validator::checkStringLength($note,1,1000)) {
+                $notes_errors["note"]="note must be no more than 1000 characters";
+            }
+    
+        }else{
+            $notes_errors["empty"]='fields cant be empty';
+        }
+
+
+
+
+        if(empty($notes_errors)) {
+            $this->editNote($noteId,$title,$note);
+        }else {
+            $_SESSION["edit_notes_errors"]=$notes_errors;
+            header("Location: ../notes");
+        }
+       
           header("Location: ../notes");
         } catch (Exception $e) {
             die("query failed" . $e->getMessage());
