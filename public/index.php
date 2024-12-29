056e0d5449a4d149cc951635b228a1aa71673501
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2024-12-29 18:57:10 +0200

    feat: refactor index.php to streamline autoloading and include session configuration

--- public/index.php
+++ public/index.php
@@ -1,15 +1,12 @@
 <?php
 const BASE_PATH = __DIR__ . '/../';
+require_once BASE_PATH . 'vendor/autoload.php';
 require_once BASE_PATH ."./core/functions.php";
 use function Core\base_path;
+require_once base_path("config/config_session.php");
 use core\Router;
 
-spl_autoload_register(function ($class){
-    require_once base_path( $class .".php");
-    });
-
 require_once base_path("./app/Models/Notes.php");
-require_once base_path("config/config_session.php");
 require_once base_path("./Core/Router.php");
 $router=new Router();
 require_once base_path("./Core/routes.php");
